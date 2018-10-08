8f74ce012b0489486517766beded65a34d2699ee
webistomin <webistomin@gmail.com>
2018-10-08 15:18:28 +0300

    Разметил формы. Подключил сторонние библиотеки

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,41 +1,34 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container fluid fill-height>
+      <v-container fluid>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
                 <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <v-btn
-                    slot="activator"
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                  >
-                    <v-icon large>code</v-icon>
-                  </v-btn>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank">
-                    <v-icon large>mdi-codepen</v-icon>
-                  </v-btn>
-                  <span>Codepen</span>
-                </v-tooltip>
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
-                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                  <v-text-field prepend-icon="person"
+                                name="username"
+                                label="Username"
+                                type="text"
+                                v-model="username">
+                  </v-text-field>
+                  <v-text-field id="password"
+                                prepend-icon="lock"
+                                name="password"
+                                label="Password"
+                                type="password"
+                                v-model="password">
+                  </v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
+                <v-btn color="primary" to="/">Login</v-btn>
+                <v-btn color="primary" to="/registration">Register</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -47,11 +40,17 @@
 
 <script>
   export default {
-    data: () => ({
-      drawer: null,
-    }),
-    props: {
-      source: String,
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
     },
   };
 </script>
+
+<style>
+  .application--wrap {
+    min-height: auto;
+  }
+</style>
--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -6,36 +6,35 @@
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <v-btn
-                    slot="activator"
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                  >
-                    <v-icon large>code</v-icon>
-                  </v-btn>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank">
-                    <v-icon large>mdi-codepen</v-icon>
-                  </v-btn>
-                  <span>Codepen</span>
-                </v-tooltip>
+                <v-toolbar-title>Register form</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
-                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                  <v-text-field prepend-icon="person"
+                                name="login"
+                                label="First name"
+                                type="text">
+                  </v-text-field>
+                  <v-text-field prepend-icon="person"
+                                name="login"
+                                label="Last name"
+                                type="text">
+                  </v-text-field>
+                  <v-text-field prepend-icon="person"
+                                name="login"
+                                label="Username"
+                                type="text">
+                  </v-text-field>
+                  <v-text-field id="password"
+                                prepend-icon="lock"
+                                name="password"
+                                label="Password"
+                                type="password">
+                  </v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
+                <v-btn color="primary">Register</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -55,3 +54,9 @@
     },
   };
 </script>
+
+<style>
+  .application--wrap {
+    min-height: auto;
+  }
+</style>
